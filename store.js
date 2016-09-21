b0d5642b603f90e67d3cf34638b321be373e5bb8
AniMag59 <la2xp1onerchar@rambler.ru>
2016-09-21 16:27:40 +0500

    Рефакторинг переносом функций на верхний уровень

--- store.js
+++ store.js
@@ -1,87 +1,75 @@
 "use strict";
 
-function statement(customer, movies, type) {
-  if(type == 'text'){
-    return statementText();
+function statementText(customer, movies) {
+  let result = `Rental Record for ${customer.name}\n`;
+  for (let rental of customer.rentals) {
+    result += `\t${getMovie(rental, movies).title}\t${getThisAmount(rental, movies)}\n`;
   }
-  if(type == 'html'){
-    return statementHtml();
-  }
-  function statementText(){
-    let result = `Rental Record for ${customer.name}\n`;
-    for (let rental of customer.rentals) {
-      result += `\t${getMovie(rental).title}\t${getThisAmount(rental)}\n`;
-    }
-    // add footer lines
-    result += `Amount owed is ${getTotalAmount(customer)}\n`;
-    result += `You earned ${getTotalRenterPoints(customer)} frequent renter points\n`;
+  // add footer lines
+  result += `Amount owed is ${getTotalAmount(customer, movies)}\n`;
+  result += `You earned ${getTotalRenterPoints(customer, movies)} frequent renter points\n`;
 
-    return result;
+  return result;
+}
+function statementHtml(customer, movies) {
+  let result = `<h1>Rental Record for ${customer.name}</h1>\n`;
+  result += '<table>\n';
+  for (let rental of customer.rentals) {
+    result += `<tr><td>${getMovie(rental, movies).title}</td><td>${getThisAmount(rental, movies)}</td></tr>\n`;
   }
-  function statementHtml(){
-    let result = `<h1>Rental Record for ${customer.name}</h1>\n`;
-    result += '<table>\n';
-    for (let rental of customer.rentals) {
-      result += `<tr><td>${getMovie(rental).title}</td><td>${getThisAmount(rental)}</td></tr>\n`;
-    }
-    result += '</table>\n';
-    // add footer lines
-    result += `<p>Amount owed is <em>${getTotalAmount(customer)}</em></p>\n`;
-    result += `<p>You earned <em>${getTotalRenterPoints(customer)}</em> frequent renter points</p>\n`;
+  result += '</table>\n';
+  // add footer lines
+  result += `<p>Amount owed is <em>${getTotalAmount(customer, movies)}</em></p>\n`;
+  result += `<p>You earned <em>${getTotalRenterPoints(customer, movies)}</em> frequent renter points</p>\n`;
 
-    return result;
+  return result;
+}
+function getTotalAmount(customer, movies) {
+  let totalAmount = 0;
+  for (let rental of customer.rentals) {
+    totalAmount += getThisAmount(rental, movies);
   }
-
-
-  function getTotalAmount(customer) {
-    let totalAmount = 0;
-    for (let rental of customer.rentals) {
-      totalAmount += getThisAmount(rental);
-    }
-    return totalAmount;
+  return totalAmount;
+}
+function getThisAmount(rental, movies){
+  let thisAmount;
+  switch (getMovie(rental, movies).code) {
+    case "regular":
+      thisAmount = 2;
+      if (rental.days > 2) {
+        thisAmount += (rental.days - 2) * 1.5;
+      }
+      break;
+    case "new":
+      thisAmount = rental.days * 3;
+      break;
+    case "childrens":
+      thisAmount = 1.5;
+      if (rental.days > 3) {
+        thisAmount += (rental.days - 3) * 1.5;
+      }
+      break;
   }
-  function getThisAmount(rental){
-    let thisAmount;
-    switch (getMovie(rental).code) {
-      case "regular":
-        thisAmount = 2;
-        if (rental.days > 2) {
-          thisAmount += (rental.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        thisAmount = rental.days * 3;
-        break;
-      case "childrens":
-        thisAmount = 1.5;
-        if (rental.days > 3) {
-          thisAmount += (rental.days - 3) * 1.5;
-        }
-        break;
-    }
-    return thisAmount;
-  }
-  function getTotalRenterPoints(customer) {
-    let TotalRenterPoints = 0;
-    for (let rental of customer.rentals) {
-      TotalRenterPoints += getRenterPoints(rental);
-    }
+  return thisAmount;
+}
+function getTotalRenterPoints(customer, movies) {
+   let TotalRenterPoints = 0;
+   for (let rental of customer.rentals) {
+      TotalRenterPoints += getRenterPoints(rental, movies);
+   }
     return TotalRenterPoints;
   }
-  function getRenterPoints(rental) {
-      if (getMovie(rental).code === "new" && rental.days > 2){
-        return 2;
-      }
-      else{
-        return 1;
-      }
-  }
-  function getMovie(rental) {
-    return movies[rental.movieID];
-  }
-
+function getRenterPoints(rental, movies) {
+    if (getMovie(rental, movies).code === "new" && rental.days > 2){
+      return 2;
+    }
+   else{
+     return 1;
+    }
 }
-
+function getMovie(rental, movies) {
+  return movies[rental.movieID];
+}
 let customer = {
   name: "martin",
   rentals: [{
@@ -105,5 +93,5 @@ let movies = {
   // etc
 };
 
-console.log(statement(customer, movies, 'text'));
-console.log(statement(customer, movies, 'html'));
+console.log(statementText(customer, movies));
+console.log(statementHtml(customer, movies));
